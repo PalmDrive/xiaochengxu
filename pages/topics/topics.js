c7c9b52cb5c9820356431aeabf29d786c8ddae9c
Brody Chen <rufengch@gmail.com>
2017-07-25 11:52:00 -0700

    Enable pullDownRefresh for topics page

--- pages/topics/topics.js
+++ pages/topics/topics.js
@@ -43,46 +43,74 @@ Page({
 
   onLoad: function () {
     const that = this;
-    const userId = Auth.getLocalUserId(),
-          userIdQuery = userId ? `?userId=` + userId : '';
-    //获取精选专题
-    wx.request({
-      url: `${app.globalData.apiBase}/topics/featured${userIdQuery}`,
-      success(res) {
-        that.setData({
-          featuredTopics: res.data.data
-        });
-      },
-      fail(res) {
-        console.log('request featured topics fail');
-      }
+    that.setData({
+      initedAt: + new Date()
     });
-    //获取首页分块专题
-    wx.request({
-      url: `${app.globalData.apiBase}/topics/homepage${userIdQuery}`,
-      success(res) {
-        const sections = res.data.data;
-        sections.forEach(section => {
-          section.topics.forEach(util.formatTopic);
-        });
-        that.setData({
-          sections
-        });
-      },
-      fail() {
-        console.log('request /topics/homepage fail');
-      }
-    });
-    //获取猜你喜欢
-    wx.request({
-      url: `${app.globalData.apiBase}/users/${userId}/suggested-topics?initedAt=${that.data.initedAt}`,
-      success(res) {
-        const topics = res.data.data;
-        topics.forEach(util.formatTopic);
-        that.setData({
-          guessTopics: topics
-        });
-      }
-    });
+    //检查storage里是否有userId，没有则请求
+    if (Auth.getLocalUserId()) {
+      init();
+    } else {
+      Auth.login(init);
+    }
+
+    function init() {
+      const userId = Auth.getLocalUserId(),
+        userIdQuery = `?userId=` + userId;
+      //获取精选专题
+      wx.request({
+        url: `${app.globalData.apiBase}/topics/featured${userIdQuery}`,
+        success(res) {
+          that.setData({
+            featuredTopics: res.data.data
+          });
+
+          wx.stopPullDownRefresh();
+        },
+        fail(res) {
+          console.log('request featured topics fail');
+        }
+      });
+      //获取首页分块专题
+      wx.request({
+        url: `${app.globalData.apiBase}/topics/homepage${userIdQuery}`,
+        success(res) {
+          const sections = res.data.data;
+          sections.forEach(section => {
+            section.topics.forEach(util.formatTopic);
+          });
+          that.setData({
+            sections
+          });
+        },
+        fail() {
+          console.log('request /topics/homepage fail');
+        }
+      });
+      //获取猜你喜欢
+      wx.request({
+        url: `${app.globalData.apiBase}/users/${userId}/suggested-topics?initedAt=${that.data.initedAt}`,
+        success(res) {
+          const topics = res.data.data;
+          topics.forEach(util.formatTopic);
+          that.setData({
+            guessTopics: topics
+          });
+        }
+      });
+    }
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.onLoad();
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    this.loadMore();
   }
 })
