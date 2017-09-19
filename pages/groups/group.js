2a56562fadf94779592528c11cd600ad98787b54
张磊-mac <676269689@qq.com>
2017-09-19 19:47:28 +0800

    解决 groups 页面 media 和 group 页面 media 不一致的问题

--- pages/groups/group.js
+++ pages/groups/group.js
@@ -79,9 +79,8 @@ Page({
   },
   //点击文章
   goToMedium: function(event) {
-    const medium = event.currentTarget.dataset.medium;
-        medium.id = 'aef0e9b0-99d4-11e7-b81d-f3e9f712edc9';
-        const  userInfo = Auth.getLocalUserInfo(),
+    const medium = event.currentTarget.dataset.medium,
+          userInfo = Auth.getLocalUserInfo(),
           gaOptions = {
             cid: Auth.getLocalUserId(),
             ec: `article_title:${medium.attributes.title},article_id:${medium.id}`,
