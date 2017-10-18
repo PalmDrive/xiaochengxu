43920f833c31f28158b5f34c581bb412aa6a6f95
徐晓珊 <1027779283@qq.com>
2017-10-18 15:40:01 +0800

    WIP

---
+++ pages/album/order.js
@@ -0,0 +1,155 @@
+//my.js
+const app = getApp(),
+  Util = require('../../utils/util'),
+  Auth = require('../../utils/auth'),
+  {request} = require('../../utils/request');
+Page({
+  data: {
+    userInfo: {},
+    userId: null,
+    loading: true,
+    favoriteTopics: [],
+    loaded: false,
+    page: {number: 1, size: 8},
+    noMore: false,
+    items: []
+  },
+  //事件处理函数
+  goToTopic: Util.goToTopic,
+
+  onLoad: function (options) {
+    // console.log('onLoad');
+    const that = this;
+
+    if (options && options.pullDown) {
+      that.setData({ 'page.number': 1, noMore: false });
+    }
+
+    Auth.getLocalUserId() && this._load();
+  },
+
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    const that = this;
+
+    Util.ga({
+      cid: Auth.getLocalUserId() || '555',
+      dp: '%2FwodeTab_XiaoChengXu',
+      dt: '已购tab页（小程序）'
+    });
+
+    if (that.data.loaded && that.data.loading) {
+      // console.log('do stuff onShow');
+      //更新订阅列表
+      const cb = topics => {
+        topics.forEach(Util.formatTopic);
+        const data = {
+          loading: false,
+          favoriteTopics: topics
+        };
+        if (topics.length < that.data.page.size) {
+          console.log('onShow no more');
+          data.noMore = true;
+        }
+        that.setData(data);
+      };
+      that.getTopics(1, cb);
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    if (this.data.loaded) {
+      this.setData({ loading: true, noMore: false, 'page.number': 1 });
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.onLoad({pullDown: true});
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    const that = this;
+    if (!this.data.loadingMore && !this.data.noMore) {
+      const pageNumber = this.data.page.number + 1;
+      that.setData({loadingMore: true, 'page.number': pageNumber});
+      const cb = topics => {
+        topics.forEach(Util.formatTopic);
+
+        const data = {
+          loadingMore: false,
+          favoriteTopics: that.data.favoriteTopics.concat(topics)
+        };
+        if (topics.length < that.data.page.size) {
+          data.noMore = true;
+        }
+        that.setData(data);
+      };
+      this.getTopics(pageNumber, cb);
+    }
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '我的订阅'
+    };
+  },
+
+  getTopics: function(pageNumber, cb) {
+    request({
+      url: `${app.globalData.apiBase}/users/${Auth.getLocalUserId()}/relationships/albums?page[size]=${this.data.page.size}&page[number]=${this.data.page.number}&fields[albums]=title,picurl&filter=unlocked`
+    }).then((res) => {
+      const topics = res.data;
+      cb(topics);
+    }, () => {
+      console.log('my page, getTopics request fail');
+    });
+  },
+  _load() {
+    const userId = Auth.getLocalUserId();
+    const cb = topics => {
+      topics.forEach(Util.formatTopic);
+
+      const data = {
+        loading: false,
+        userInfo: Auth.getLocalUserInfo().attributes,
+        userId,
+        favoriteTopics: topics,
+        loaded: true
+      };
+      if (topics.length < this.data.page.size) {
+        data.noMore = true;
+      }
+      this.setData(data);
+      wx.stopPullDownRefresh();
+    };
+    this.getTopics(1, cb);
+  }
+})
