983406c175b1cba882c129b15969a1dfda4b437a
徐晓珊 <1027779283@qq.com>
2017-11-10 14:31:45 +0800

    完善任务页面

--- pages/album/survey.js
+++ pages/album/survey.js
@@ -4,18 +4,22 @@ const app = getApp(),
     {request} = require('../../utils/request'),
     baseUrl = app.globalData.apiBase;
 
+let albumId = undefined,
+    postId = undefined;
+
 Page({
   data: {
-    dayList: ['1', '2', '3', '4', '5', '6','7'],
+    dayList: [],
     selectedIndex: 4,
     albumAttributes: {},
-    albumId: '',
     editorInfo: {},
     post: {},
     media: []
   },
 
   onLoad(options) {
+    albumId = options.albumId;
+    postId = options.postId;
     Auth.getLocalUserId() && this._load();
   },
 
@@ -23,20 +27,27 @@ Page({
    * 加载数据
    */
   _load() {
+    let url = `${app.globalData.apiBase}/albums/post`;
+    if (albumId && postId) {
+      url += `?postId=${postId}&albumId=${albumId}`
+    }
     request({
-      url: `${app.globalData.apiBase}/albums/post`,
+      url: url,
     }).then(res => {
       console.log(res);
 
-      const albumAttributes = res.data.attributes;
-      const post = res.data.relationships.post;
-
+      const albumAttributes = res.data.attributes || {},
+            post = res.data.relationships.post.data || {},
+            postRelationships = post.relationships || {};
+      albumId = res.data.id;
+      postId = post.id;
       this.setData({
         albumAttributes,
         editorInfo: albumAttributes.editorInfo,
         post,
-        media: post.relationships.media.data,
-        albumId: res.data.id
+        media: postRelationships.media.data,
+        selectedIndex: post.attributes.dayIndex,
+        dayList: res.meta.checkinStatus
       })
     });
   },
@@ -50,12 +61,19 @@ Page({
       title: `七日辑: ${title}`
     };
   },
+  goToPost: function(event) {
+    const index = event.currentTarget.dataset.index,
+          newPostId = this.data.albumAttributes.postIds[index]
+    wx.redirectTo({
+      url: `./survey?postId=${newPostId}&albumId=${albumId}`
+    });
+  },
 
   //点击文章
   goToMedium: function(event) {
     const medium = event.currentTarget.dataset.medium,
           index = this.data.selectedIndex,
-          idx = event.currentTarget.dataset.index,
+          idx = event.currentTarget.dataset.idx,
           count = this.data.media.length,
           userInfo = Auth.getLocalUserInfo(),
           gaOptions = {
@@ -65,18 +83,19 @@ Page({
             el: `album_name:${this.data.albumAttributes.title},album_id:${this.data.albumId}`,
             ev: 0
           };
-    const key = 'day' + (this.data.posts.length - index);
-    if (!this.data.dayLogs[key]) {
-      this.data.dayLogs[key] = 1;
-      this.data.achieveProcess = this.data.achieveProcess + 1;
-      this.setData({achieveProcess: this.data.achieveProcess});
-    }
+    const key = 'day' + index;
     util.goToMedium(event, gaOptions, {
       dayIndex: key,
       mediumIndex: idx + 1,
       count: count,
-      albumId: this.data.albumId,
-      morningPostId: this._getMorningPostId()
+      albumId: albumId,
+      morningPostId: postId
     });
   },
+
+  goToAlbumDetail: function(event) {
+    wx.redirectTo({
+      url: `./show?showDetail=true&id=${albumId}`
+    });
+  }
 })
