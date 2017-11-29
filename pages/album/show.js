1572439b303f9eec9d830d71c79be8b829c42fd9
徐晓珊 <1027779283@qq.com>
2017-11-29 18:43:54 +0800

    groupQrCodeMediaIds 改为小写的

--- pages/album/show.js
+++ pages/album/show.js
@@ -314,7 +314,7 @@ Page({
       Auth.setLocalKey(`${this.data.albumId}_hasShownSubscribedWX`, 'true');
       // 关注过服务号, 弹出微信群二维码
       if (Auth.getLocalKey('isSubscribedWX') === 'true') {
-        const groupQrcodes = metaData.groupQRCodeMediaIds || [],
+        const groupQrcodes = metaData.groupQrCodeMediaIds || [],
               showWxQrcode = attributes.programStartAt ? true : false,
               newGroupQrcodes = groupQrcodes.filter(item => {
                 return item.active;
