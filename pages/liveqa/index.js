960731ca1d22c4a69a1c9e5ab9b8a2ac27b61cdc
Yujun Wu <wyj0912@gmail.com>
2018-03-01 12:12:47 +0800

    Add school selector

--- pages/liveqa/index.js
+++ pages/liveqa/index.js
@@ -1,16 +1,31 @@
 const mockData = require('../../utils/mockData'),
       utils = require('../../utils/util'),
       Auth = require('../../utils/auth'),
-      _ = require('../../vendors/underscore');
+      _ = require('../../vendors/underscore'),
+      graphql = require('../../utils/graphql');
 
-Page({
+function querySchoolList(name) {
+  return graphql(`query {
+    schoolList (name: "${name}") {
+      id, name
+    }
+  }`)
+    .then(res => res.data.schoolList);
+}
+
+const page = Page({
   data: {
     live: null,
     user: null,
-    liveSchools: null
+    liveSchools: null,
+
+    //schoolList: [{id: 1, name: 'Stanford'}, {id: 2, name: '上海交通大学'}, {id: 3, name: 'Purdue University'}],
+    schoolList: [], // for user select school
+    selectedSchool: null
   },
 
   onLoad(options) {
+    wx.showLoading({title: '', mask: true});
     this._fetchData()
       .then(data => {
         this.setData({
@@ -18,19 +33,76 @@ Page({
           user: data.user,
           liveSchools: data.liveSchools
         });
-
-        this._timeToNextLiveCountDown();
+        wx.hideLoading();
+        //this._timeToNextLiveCountDown();
       });
   },
 
-  _fetchData() {
-    const user = Auth.getLocalUserInfo();
-    return new Promise(resolve => {
-      _.extend(mockData.user, user.attributes, {id: user.id});
-      this._setTimeToNextLive(mockData.live);
-      mockData.live.schedule = this._formatSchedule(mockData.live);
-      resolve(mockData);
+  startQA() {
+    wx.navigateTo({
+      url: `./questions`
     });
+  },
+
+  inputSchoolName: _.debounce(function(e) { // 必须是function, 不能用 =>
+    const name = e.detail.value;
+    //console.log(`school: ${name}`);
+    querySchoolList(name)
+      .then(data => {
+        this.setData({schoolList: data});
+      });
+  }, 500),
+
+  selectSchool(e) {
+    const school = e.target.dataset.item;
+    this.setData({
+      selectedSchool: school,
+      schoolList: []
+    });
+  },
+
+  saveSelectedSchool() {
+    const user = this.data.user,
+          school = this.data.selectedSchool;
+    wx.showLoading({title: '', mask: true});
+    return graphql(`mutation {
+      user (id: "${user.id}", schoolId: "${school.id}") {
+        id
+      },
+      liveSchool (schoolId: "${school.id}", name: "${school.name}") {
+        id
+      }
+    }`)
+      .then(() => this.onLoad());
+  },
+
+  _fetchData() {
+    const user = Auth.getLocalUserInfo(),
+          query = `query {
+            liveSchools (order: [["points", "DESC"]]) {
+              id, name, ranking, registeredUsersCount, points
+            },
+            users (id: "${user.id}") {
+              myLiveSchool {
+                name, rtRanking
+              },
+              extraQALives
+            }
+          }`;
+
+    return graphql(query)
+      .then(res => {
+        const userData = res.data.users[0],
+              data = {
+                user: _.extend(userData, user.attributes, {id: user.id}),
+                liveSchools: res.data.liveSchools
+              };
+
+        //this._setTimeToNextLive(mockData.live);
+        //mockData.live.schedule = this._formatSchedule(mockData.live);
+
+        return data;
+      });
   },
 
   _formatSchedule(live) {
@@ -70,5 +142,5 @@ Page({
          });
        }
      }, 1000);
-  }
+  },
 });
