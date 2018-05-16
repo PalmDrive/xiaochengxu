5575a52b2893a681e0ea306f7f1186f6270237b6
徐晓珊 <1027779283@qq.com>
2018-05-16 14:52:06 +0800

    增加小程序购买课程功能

--- pages/liveqa/course.js
+++ pages/liveqa/course.js
@@ -8,14 +8,22 @@ Page({
     url: ''
   },
 
+  onShow: function () {
+    console.log('on show')
+    const paySuccessUrl = getApp().globalData.paySuccessUrl
+    getApp().globalData.paySuccessUrl=''
+    if (paySuccessUrl) {
+      this.setData({
+        url: paySuccessUrl
+      })
+    }
+  },
+
   onLoad(options) {
     wx.setNavigationBarTitle({title: '集思学院课程详情'});
-    const url =
-    `https://www.gecacademy.cn/#/chip?id=${options.courseId}`;
-    console.log('web url:', url);
-
+    // wx.navigateTo({url: `/pages/liveqa/pay?orderId=1526437080dr3dk6ac1vxdho94fgvi`});
     this.setData({
-      url
+      url: `https://www.gecacademy.cn/#/chip?id=${options.courseId}`
     });
   }
 })
---
+++ pages/liveqa/pay.js
@@ -0,0 +1,40 @@
+const utils = require('../../utils/util'),
+      Auth = require('../../utils/auth'),
+      _ = require('../../vendors/underscore'),
+      graphql = require('../../utils/graphql');
+
+Page({
+  data: {
+    message: '支付中'
+  },
+
+  onLoad(options) {
+    wx.setNavigationBarTitle({title: '支付中'});
+    getApp().globalData.paySuccessUrl = `https://gecacademy.cn/#/success?orderId=${options.orderId}`
+    // wx.navigateBack();
+    const params = {
+      timeStamp: options.timeStamp,
+      nonceStr: options.nonceStr,
+      package: `prepay_id=${options.prepay_id}`,
+      signType: 'MD5',
+      paySign: options.paySign
+    };
+
+    params.success = (res) => {
+      console.log('wx requestPayment success');
+      console.log(res);
+      this.setData({
+        message: '支付成功'
+      });
+      wx.navigateBack();
+    };
+    params.fail = (err) => {
+      console.log('wx requestPayment fail');
+      console.log(err);
+    };
+    params.complete = () => {
+      console.log('wx requestPayment complete');
+    };
+    wx.requestPayment(params);
+  }
+});
